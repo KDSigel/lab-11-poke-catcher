f2177e8480707bb0d529179477dd0670083235fd
Karl Sigel <heykarl@gmail.com>
2021-08-31 13:35:14 -0700

    added some html

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
+import { getRandomPokemon } from './utils.js';
+import pokemon from './data.js';
 
+const catchBtn = document.getElementById('catch-btn');
 // initialize global state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+  // User clicks catch button
+catchBtn.addEventListener('click', () => {
+});
+  // - increment: `pokeCaught++`
+  // - We figure out the id of the pokemon that was captured.
+  //     - call `catchPokemon(id)` with this id
+  // - now, if `pokeCaught > 10`, redirect to the results page
+  // - call `renderNewPokemon()`
