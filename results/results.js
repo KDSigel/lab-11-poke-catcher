a07536431889cf3e4e1e8d2fac52c49a3fcfcf21
Karl Sigel <heykarl@gmail.com>
2021-09-01 15:01:02 -0700

    added captures to existing chart

--- results/results.js
+++ results/results.js
@@ -17,13 +17,29 @@ const myChart = new Chart(ctx, { //eslint-disable-line
             label: '# of encounters',
             data: numberOfEncounters,
             backgroundColor: [
-                'rgba(0, 222, 8, 0.2)',
-                'rgba(22, 201, 55, 0.2)',
-                'rgba(44, 188, 86, 0.2)',
-                'rgba(88, 144, 122, 0.2)',
-                'rgba(122, 122, 188, 0.2)',
-                'rgba(144, 88, 205, 0.2)',
-                'rgba(188, 44, 222, 0.2)'
+                'rgba(80, 111, 88, 0.1)',
+                'rgba(0, 222, 8, 0.1)',
+                'rgba(22, 0, 55, 0.1)',
+                'rgba(44, 188, 86, 0.1)',
+                'rgba(88, 144, 0, 0.1)',
+                'rgba(122, 122, 188, 0.1)',
+                'rgba(144, 88, 205, 0.1)',
+                'rgba(0, 44, 222, 0.1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: '# of captures',
+            data: numberOfCaptures,
+            backgroundColor: [
+                'rgba(80, 111, 88, 0.7)',
+                'rgba(0, 222, 8, 0.7)',
+                'rgba(22, 0, 55, 0.7)',
+                'rgba(44, 188, 86, 0.7)',
+                'rgba(88, 144, 0, 0.7)',
+                'rgba(122, 122, 188, 0.7)',
+                'rgba(144, 88, 205, 0.7)',
+                'rgba(0, 44, 222, 0.7)'
             ],
             borderWidth: 1
         }], 
